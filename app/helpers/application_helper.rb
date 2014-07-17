f79e8d45c77d105915c575cad2ca6a3b29bfd9e0
Nate Hill <natanio@gmail.com>
2014-07-17 09:39:43 +0900

    Add name fields to user and form to HP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
   def show_video(url)
     render :partial => 'shared/video', :locals => { :url => url }
   end 
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
