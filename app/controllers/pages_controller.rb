bad04d43330f1128d0fd4e2a572cb37f1d2ee4b6
Nate Hill <natanio@gmail.com>
2014-08-26 07:30:49 +0900

    Add restrictions to dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
 	before_filter :authenticate_user!, only: [:dashboard]
+	before_filter :check_subscription, only: [:dashboard]
 	
   def about
   	render :layout => "home"
@@ -10,5 +11,11 @@ class PagesController < ApplicationController
 
   def dashboard
   end
+
+  def check_subscription
+      if user_signed_in? && !current_user.active_subscription == true
+        redirect_to new_charge_path, alert: "Before you can access your dashboard, you need to choose a plan. Thanks!"
+      end
+    end
     
 end
