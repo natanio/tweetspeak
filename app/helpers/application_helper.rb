e2a4f65fee96099724d58ee40dfadb55434eebfe
Nate Hill <natanio@gmail.com>
2014-09-25 11:14:36 +0900

    Create dynamic meta description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,12 @@ module ApplicationHelper
     end
   end
 
+  def meta_description(desc = nil)
+    if desc.present?
+      content_for :meta_description, desc
+    else
+      content_for?(:meta_description) ? content_for(:meta_description) : APP_CONFIG['meta_description']
+    end
+  end
+
 end
