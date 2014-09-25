11f4a74ecd679e5f52b591d2c7234dbfbce95f4d
Nate Hill <natanio@gmail.com>
2014-09-25 14:02:01 +0900

    Add sendgrid for mailer

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,13 @@ APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")
 
 # Initialize the Rails application.
 Tweetspeak::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
