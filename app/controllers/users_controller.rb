27c169d819971ed9685f078a52e0d0add932be48
Nate Hill <natanio@gmail.com>
2014-10-17 05:40:53 +0900

    finish users index page for admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
+	before_filter :check_user, only: [:index]
 	
   def index
     @users = User.all
   end
+
+  private
+
+  def check_user
+      if user_signed_in?
+      	unless current_user.admin? 
+        redirect_to pages_dashboard_path, alert: "Sorry, that page is not for students."
+        end
+      else
+      	 redirect_to root_path, alert: "Sorry, you need to sign up or sign in to visit that page."
+      end
+    end
 
 end
