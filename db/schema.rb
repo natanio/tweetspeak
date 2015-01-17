a8e40698b54d8c4788a09eddf4c95a0e91a86a78
Nate Hill <natanio@gmail.com>
2015-01-17 12:42:40 +0900

    add english name to languages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150116015924) do
+ActiveRecord::Schema.define(version: 20150117020650) do
 
   create_table "cards", force: true do |t|
     t.string   "title"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150116015924) do
 
   create_table "languages", force: true do |t|
     t.string "name"
+    t.string "english_name"
   end
 
   create_table "lessons", force: true do |t|
